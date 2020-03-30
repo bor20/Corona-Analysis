5c73dfebfd9b471683042a517d48f852de1aacb8
bor20 <39258132+bor20@users.noreply.github.com>
2020-03-30 16:59:51 +0300

    update as of 2

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-import pandas as pd 
+import pandas as pd
 import random
 import math
 import time
@@ -12,12 +12,15 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import datetime
-import operator 
+import operator
 plt.style.use('seaborn')
 
-confirmed_df = pd.read_csv('novel-corona-virus-2019-dataset/time_series_covid_19_confirmed.csv')
-deaths_df = pd.read_csv('novel-corona-virus-2019-dataset/time_series_covid_19_deaths.csv')
-recoveries_df = pd.read_csv('novel-corona-virus-2019-dataset/time_series_covid_19_recovered.csv')
+confirmed_df = pd.read_csv(
+    'novel-corona-virus-2019-dataset/time_series_covid_19_confirmed.csv')
+deaths_df = pd.read_csv(
+    'novel-corona-virus-2019-dataset/time_series_covid_19_deaths.csv')
+recoveries_df = pd.read_csv(
+    'novel-corona-virus-2019-dataset/time_series_covid_19_recovered.csv')
 
 confirmed_df.head()
 
@@ -28,34 +31,41 @@ recoveries = recoveries_df.loc[:, cols[4]:cols[-1]]
 
 dates = confirmed.keys()
 world_cases = []
-total_deaths = [] 
+total_deaths = []
 mortality_rate = []
-recovery_rate = [] 
-total_recovered = [] 
-total_active = [] 
-china_cases = [] 
+recovery_rate = []
+total_recovered = []
+total_active = []
+china_cases = []
 italy_cases = []
-us_cases = [] 
+us_cases = []
+spain_cases = []
 
 for i in dates:
     confirmed_sum = confirmed[i].sum()
     death_sum = deaths[i].sum()
     recovered_sum = recoveries[i].sum()
-    
+
     # confirmed, deaths, recovered, and active
     world_cases.append(confirmed_sum)
     total_deaths.append(death_sum)
     total_recovered.append(recovered_sum)
     total_active.append(confirmed_sum-death_sum-recovered_sum)
-    
+
     # calculate rates
     mortality_rate.append(death_sum/confirmed_sum)
     recovery_rate.append(recovered_sum/confirmed_sum)
 
-    # case studies 
-    china_cases.append(confirmed_df[confirmed_df['Country/Region']=='China'][i].sum())
-    italy_cases.append(confirmed_df[confirmed_df['Country/Region']=='Italy'][i].sum())
-    us_cases.append(confirmed_df[confirmed_df['Country/Region']=='US'][i].sum())
+    # case studies
+    china_cases.append(
+        confirmed_df[confirmed_df['Country/Region'] == 'China'][i].sum())
+    italy_cases.append(
+        confirmed_df[confirmed_df['Country/Region'] == 'Italy'][i].sum())
+    us_cases.append(
+        confirmed_df[confirmed_df['Country/Region'] == 'US'][i].sum())
+    spain_cases.append(
+        confirmed_df[confirmed_df['Country/Region'] == 'Spain'][i].sum())
+
 
 days_since_1_22 = np.array([i for i in range(len(dates))]).reshape(-1, 1)
 world_cases = np.array(world_cases).reshape(-1, 1)
@@ -63,16 +73,19 @@ total_deaths = np.array(total_deaths).reshape(-1, 1)
 total_recovered = np.array(total_recovered).reshape(-1, 1)
 
 days_in_future = 30
-future_forcast = np.array([i for i in range(len(dates)+days_in_future)]).reshape(-1, 1)
+future_forcast = np.array(
+    [i for i in range(len(dates)+days_in_future)]).reshape(-1, 1)
 adjusted_dates = future_forcast[:-30]
 
 start = '1/22/2020'
 start_date = datetime.datetime.strptime(start, '%m/%d/%Y')
 future_forcast_dates = []
 for i in range(len(future_forcast)):
-    future_forcast_dates.append((start_date + datetime.timedelta(days=i)).strftime('%m/%d/%Y'))
+    future_forcast_dates.append(
+        (start_date + datetime.timedelta(days=i)).strftime('%m/%d/%Y'))
 
-X_train_confirmed, X_test_confirmed, y_train_confirmed, y_test_confirmed = train_test_split(days_since_1_22, world_cases, test_size=0.15, shuffle=False) 
+X_train_confirmed, X_test_confirmed, y_train_confirmed, y_test_confirmed = train_test_split(
+    days_since_1_22, world_cases, test_size=0.15, shuffle=False)
 
 # use this to find the optimal parameters for SVR
 # c = [0.01, 0.1, 1]
@@ -90,7 +103,8 @@ X_train_confirmed, X_test_confirmed, y_train_confirmed, y_test_confirmed = train
 # svm_search.best_params_
 
 # svm_confirmed = svm_search.best_estimator_
-svm_confirmed = SVR(shrinking=True, kernel='poly',gamma=0.01, epsilon=1,degree=6, C=0.1)
+svm_confirmed = SVR(shrinking=True, kernel='poly',
+                    gamma=0.01, epsilon=1, degree=6, C=0.1)
 svm_confirmed.fit(X_train_confirmed, y_train_confirmed)
 svm_pred = svm_confirmed.predict(future_forcast)
 
@@ -99,7 +113,7 @@ svm_test_pred = svm_confirmed.predict(X_test_confirmed)
 plt.plot(svm_test_pred)
 plt.plot(y_test_confirmed)
 print('MAE:', mean_absolute_error(svm_test_pred, y_test_confirmed))
-print('MSE:',mean_squared_error(svm_test_pred, y_test_confirmed))
+print('MSE:', mean_squared_error(svm_test_pred, y_test_confirmed))
 
 # transform our data for polynomial regression
 poly = PolynomialFeatures(degree=5)
@@ -113,23 +127,25 @@ linear_model.fit(poly_X_train_confirmed, y_train_confirmed)
 test_linear_pred = linear_model.predict(poly_X_test_confirmed)
 linear_pred = linear_model.predict(poly_future_forcast)
 print('MAE:', mean_absolute_error(test_linear_pred, y_test_confirmed))
-print('MSE:',mean_squared_error(test_linear_pred, y_test_confirmed))
+print('MSE:', mean_squared_error(test_linear_pred, y_test_confirmed))
 
 print(linear_model.coef_)
 plt.plot(test_linear_pred)
 plt.plot(y_test_confirmed)
 
-# bayesian ridge 
+# bayesian ridge
 tol = [1e-4, 1e-3, 1e-2]
 alpha_1 = [1e-7, 1e-6, 1e-5, 1e-4]
 alpha_2 = [1e-7, 1e-6, 1e-5, 1e-4]
 lambda_1 = [1e-7, 1e-6, 1e-5, 1e-4]
 lambda_2 = [1e-7, 1e-6, 1e-5, 1e-4]
 
-bayesian_grid = {'tol': tol, 'alpha_1': alpha_1, 'alpha_2' : alpha_2, 'lambda_1': lambda_1, 'lambda_2' : lambda_2}
+bayesian_grid = {'tol': tol, 'alpha_1': alpha_1,
+                 'alpha_2': alpha_2, 'lambda_1': lambda_1, 'lambda_2': lambda_2}
 
 bayesian = BayesianRidge(fit_intercept=False, normalize=True)
-bayesian_search = RandomizedSearchCV(bayesian, bayesian_grid, scoring='neg_mean_squared_error', cv=3, return_train_score=True, n_jobs=-1, n_iter=40, verbose=1)
+bayesian_search = RandomizedSearchCV(bayesian, bayesian_grid, scoring='neg_mean_squared_error',
+                                     cv=3, return_train_score=True, n_jobs=-1, n_iter=40, verbose=1)
 bayesian_search.fit(X_train_confirmed, y_train_confirmed)
 
 bayesian_search.best_params_
@@ -138,7 +154,7 @@ bayesian_confirmed = bayesian_search.best_estimator_
 test_bayesian_pred = bayesian_confirmed.predict(X_test_confirmed)
 bayesian_pred = bayesian_confirmed.predict(future_forcast)
 print('MAE:', mean_absolute_error(test_bayesian_pred, y_test_confirmed))
-print('MSE:',mean_squared_error(test_bayesian_pred, y_test_confirmed))
+print('MSE:', mean_squared_error(test_bayesian_pred, y_test_confirmed))
 
 plt.figure(figsize=(16, 9))
 plt.plot(adjusted_dates, world_cases)
@@ -153,10 +169,13 @@ plt.figure(figsize=(16, 9))
 plt.plot(adjusted_dates, china_cases)
 plt.plot(adjusted_dates, italy_cases)
 plt.plot(adjusted_dates, us_cases)
+plt.plot(adjusted_dates, spain_cases)
+
+
 plt.title('# of Coronavirus Cases', size=30)
 plt.xlabel('Days Since 1/22/2020', size=30)
 plt.ylabel('# of Cases', size=30)
-plt.legend(['China', 'Italy', 'US'], prop={'size': 20})
+plt.legend(['China', 'Italy', 'US', 'Spain'], prop={'size': 20})
 plt.xticks(size=20)
 plt.yticks(size=20)
 plt.show()
@@ -178,7 +197,8 @@ plt.plot(future_forcast, linear_pred, linestyle='dashed', color='orange')
 plt.title('# of Coronavirus Cases Over Time', size=30)
 plt.xlabel('Days Since 1/22/2020', size=30)
 plt.ylabel('# of Cases', size=30)
-plt.legend(['Confirmed Cases', 'Polynomial Regression Predictions'], prop={'size': 20})
+plt.legend(['Confirmed Cases', 'Polynomial Regression Predictions'],
+           prop={'size': 20})
 plt.xticks(size=20)
 plt.yticks(size=20)
 plt.show()
@@ -189,21 +209,22 @@ plt.plot(future_forcast, bayesian_pred, linestyle='dashed', color='green')
 plt.title('# of Coronavirus Cases Over Time', size=30)
 plt.xlabel('Time', size=30)
 plt.ylabel('# of Cases', size=30)
-plt.legend(['Confirmed Cases', 'Bayesian Ridge Regression Predictions'], prop={'size': 20})
+plt.legend(['Confirmed Cases', 'Bayesian Ridge Regression Predictions'],
+           prop={'size': 20})
 plt.xticks(size=20)
 plt.yticks(size=20)
 plt.show()
 
-# Future predictions using SVM 
+# Future predictions using SVM
 print('SVM future predictions:')
 set(zip(future_forcast_dates[-10:], np.round(svm_pred[-10:])))
 
-# Future predictions using Polynomial Regression 
-linear_pred = linear_pred.reshape(1,-1)[0]
+# Future predictions using Polynomial Regression
+linear_pred = linear_pred.reshape(1, -1)[0]
 print('Polynomial regression future predictions:')
 set(zip(future_forcast_dates[-10:], np.round(linear_pred[-10:])))
 
-# Future predictions using Linear Regression 
+# Future predictions using Linear Regression
 print('Ridge regression future predictions:')
 set(zip(future_forcast_dates[-10:], np.round(bayesian_pred[-10:])))
 
